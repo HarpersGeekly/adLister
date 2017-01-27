cda2998e0353c689a39bde33131153cefd095e83
HarpersGeekly <HarperRyanC@gmail.com>
2017-01-27 17:01:38 -0600

    Servlets exercise 'Hello World'

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,32 @@
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
+import java.io.*;
+import java.util.Scanner;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet(name = "HelloServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+        Object counter = request.getAttribute("counter");
+        int count;
+
+        if (counter != null) {
+            count = Integer.parseInt((String) counter);
+            count++;
+
+        } else {
+            count = 1;
+        }
+
+        request.setAttribute("counter", count);
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello, " + getServletName() + "</h1>");
+        out.println("<p>Page Counter: " + count + "</p>");
 
     }
 }
