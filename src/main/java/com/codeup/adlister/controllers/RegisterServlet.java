bda5e8b3603eae9024985cc1add55fe71da637a2
HarpersGeekly <HarperRyanC@gmail.com>
2017-02-06 09:23:39 -0600

    Add validation to the register page
    -checked all the values in the form are present and they are not empty.
    -Used forward instead of redirect in the LoginServlet

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -16,26 +16,35 @@ public class RegisterServlet extends HttpServlet {
             HttpServletRequest request,
             HttpServletResponse response)
             throws ServletException, IOException {
+
         // TODO: show the registration form
         request.getRequestDispatcher("/WEB-INF/users/register.jsp").forward(request, response);
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doPost(
+            HttpServletRequest request,
+            HttpServletResponse response)
+            throws IOException, ServletException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         String email = request.getParameter("email");
+
         // TODO: ensure the submitted information is valid
+        if (username !=null && password != null && email != null
+            && !username.isEmpty() && !password.isEmpty() && !email.isEmpty()) {
 
+            // TODO: create a new user based off of the submitted information
+            // a new user will have a 0 id..
+            User user = new User(0, username, email, password);
 
-        // TODO: create a new user based off of the submitted information
-        // a new user will have a 0 id..
-        User user = new User(0, username, email, password);
+            // TODO: if a user was successfully created, send them to their profile
 
-        // TODO: if a user was successfully created, send them to their profile
-
-        DaoFactory.getUsersDao().insert(user);
-        //allow for automatic login:
-        request.getSession().setAttribute("user", username);
-        response.sendRedirect("/profile");
+            DaoFactory.getUsersDao().insert(user);
+            //allow for automatic login:
+            request.getSession().setAttribute("user", user);
+            response.sendRedirect("/profile");
+        } else {
+            doGet(request, response); //otherwise, show the register form again
+        }
     }
 }
